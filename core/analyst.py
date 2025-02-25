0cea287dc86194d001aefdff0d3782c889528b25
Ádám Groszeibl <adamgroszeibl@Adams-MacBook-Pro.local>
2025-02-25 18:22:54 +0000

    reverting iqr cleanup

--- core/analyst.py
+++ core/analyst.py
@@ -82,18 +82,7 @@ class Analyst:
             FROM base
         '''
 
-        # performing iqr cleanup to smooth out the trend
-        df = self.db_handler.fetch_df(query)
-        q1 = df["conversion_rate"].quantile(0.25)
-        q3 = df["conversion_rate"].quantile(0.75)
-        iqr = q3 - q1
-
-        lower_bound = q1 - 1.5 * iqr
-        upper_bound = q1 + 1.5 * iqr
-
-        df_clean = df[(df["conversion_rate"] >= lower_bound) & (df["conversion_rate"] <= upper_bound)]
-
-        return df_clean
+        return self.db_handler.fetch_df(query)
     
     def conversion_rates_by_page_cat_agg(self):
         '''
